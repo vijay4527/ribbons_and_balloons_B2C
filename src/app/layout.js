412dd181671dc441bd320955e68d88d1548aacd2
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-13 10:40:48 +0530

    layout changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 "use client"
-import { useEffect } from 'react';
 import Navbar from "@/components/nav";
 import "./globals.css";
 import { Inter, Roboto, Poppins } from "next/font/google";
@@ -14,22 +13,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    const bootstrapScript = document.createElement('script');
-    bootstrapScript.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha2/dist/js/bootstrap.bundle.min.js';
-    bootstrapScript.async = true;
-    document.body.appendChild(bootstrapScript);
-
-    const jqueryScript = document.createElement('script');
-    jqueryScript.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-    jqueryScript.async = true;
-    document.body.appendChild(jqueryScript);
-
-    return () => {
-      document.body.removeChild(bootstrapScript);
-      document.body.removeChild(jqueryScript);
-    };
-  }, []);
+ 
 
   return (
     <html lang="en">
