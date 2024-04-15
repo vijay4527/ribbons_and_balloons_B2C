0f4fd1f332241c9d459d0387ce405cf263f7029b
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-15 13:24:18 +0530

    links added to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,26 @@ export const metadata = {
   description: "This is the description",
 };
 
-export default function RootLayout({ children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+        />
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <Navbar />
