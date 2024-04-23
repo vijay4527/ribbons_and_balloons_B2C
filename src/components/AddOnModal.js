57b9e7f6cdc94945ec4c7b72a70717cb9e6ddec4
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-23 17:41:45 +0530

    new changes

--- src/components/AddOnModal.js
+++ src/components/AddOnModal.js
@@ -1,17 +1,16 @@
-"use client";
-import React from "react";
-import { axiosPost, axiosGet } from "@/api";
-import homeStyles from "@/app/home.module.css";
+import React, { useState, useEffect } from "react";
+import { axiosPost } from "@/api";
 import Modal from "react-bootstrap/Modal";
-import { useState, useEffect } from "react";
-import styles from "@/app/[city]/l/[category]/search.module.css";
 import Link from "next/link";
 import AppConfig from "@/AppConfig";
+import homeStyles from "@/app/home.module.css";
+
 const AddOnModal = ({ isOpen, onRequestClose, closeModal, city }) => {
   const [modalIsOpen, setModalIsOpen] = useState(isOpen);
-  const [isSelected, setIsSelected] = useState(false);
   const [addOns, setAddOns] = useState([]);
-  const [quantity,setQuantity] = useState(1)
+  const [selectedIndexes, setSelectedIndexes] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+
   useEffect(() => {
     setModalIsOpen(true);
     getAddOns();
@@ -27,23 +26,32 @@ const AddOnModal = ({ isOpen, onRequestClose, closeModal, city }) => {
     }
   };
 
+  const toggleSelection = (index) => {
+    const updatedIndexes = [...selectedIndexes];
+    if (updatedIndexes.includes(index)) {
+      updatedIndexes.splice(updatedIndexes.indexOf(index), 1);
+    } else {
+      updatedIndexes.push(index);
+    }
+    setSelectedIndexes(updatedIndexes);
+  };
+
   const decrementQuantity = () => {
     if (quantity > 0) {
       setQuantity((prevQuantity) => prevQuantity - 1);
-    
     }
     if (quantity < 1) {
-        setIsSelected(false);
-      }
+        setQuantity(1)
+      setSelectedIndexes([]);
+    }
   };
-  
-  
-  
+
   const incrementQuantity = () => {
     if (quantity < 10) {
       setQuantity((prevQuantity) => prevQuantity + 1);
     }
   };
+
   return (
     <Modal
       id="login-modal-dialog"
@@ -58,8 +66,8 @@ const AddOnModal = ({ isOpen, onRequestClose, closeModal, city }) => {
         <div class="addon-details">
           <div class="addon-description">
             {addOns && addOns.length > 0
-              ? addOns.map((item) => (
-                  <div class="addon-card">
+              ? addOns.map((item, index) => (
+                  <div class="addon-card" key={index}>
                     <div class="addon-image">
                       <img
                         src={AppConfig.cdn + "products/" + item.addon_image}
@@ -70,32 +78,36 @@ const AddOnModal = ({ isOpen, onRequestClose, closeModal, city }) => {
                     </div>
                     <div class="title-price-icon mt-2">
                       <div class="title-price">
-                        <span class="addon-title" title={item.addon_name}>{item.addon_name}</span>
+                        <span class="addon-title" title={item.addon_name}>
+                          {item.addon_name}
+                        </span>
                         <span class="addon-price">
                           <img
                             src="https://media.bakingo.com/bakingo-ssr/static/media/rupees.ac282a46.svg"
                             width="0"
                             height="0"
-                          />
-                          {" "}
-                          {item.cost}
-                          {" "}
+                          />{" "}
+                          {item.cost}{" "}
                         </span>
                       </div>
-                      {!isSelected && (
+                      {!selectedIndexes.includes(index) && (
                         <div class="add_icon_image">
                           <img
                             src="https://media.bakingo.com/bakingo-ssr/static/media/add_icon.1b323ad6.svg"
                             alt="add"
-                            onClick={()=>setIsSelected(true)}
+                            onClick={() => toggleSelection(index)}
                           />
                         </div>
                       )}
-                      {isSelected && (
+                      {selectedIndexes.includes(index) && (
                         <div class="addon-quantity-show">
-                          <span class="addon-sub" onClick={decrementQuantity}>- </span>
+                          <span class="addon-sub" onClick={decrementQuantity}>
+                            -{" "}
+                          </span>
                           <span class="addon-add-sub">{quantity}</span>
-                          <span class="addon-add" onClick={incrementQuantity}>+ </span>
+                          <span class="addon-add" onClick={incrementQuantity}>
+                            +{" "}
+                          </span>
                         </div>
                       )}
                     </div>
@@ -104,13 +116,13 @@ const AddOnModal = ({ isOpen, onRequestClose, closeModal, city }) => {
               : ""}
           </div>
         </div>
-
       </div>
       <div class="text-center">
-        <Link href={`/${city}/cart`}> <div class="btn btn-secondary mb-4">Skip</div></Link>
-       
-        {" "}{" "}<div class="btn btn-primary mb-4">Continue</div></div>
-
+        <Link href={`/${city}/cart`}>
+          <div class="btn btn-secondary mb-4">Skip</div>
+        </Link>{" "}
+        <div class="btn btn-primary mb-4">Continue</div>
+      </div>
     </Modal>
   );
 };
