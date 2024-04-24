f1d142f3192f382378a11c3433909dea195b9c17
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-24 12:22:39 +0530

    validation added to login part

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,18 +1,28 @@
 import * as yup from "yup";
 
 const loginSchema = yup.object().shape({
-  mobile: yup.string().required("Mobile or Email is required"),
- 
+  mobile: yup
+    .number()
+    .typeError("Mobile number must be a number")
+    .positive("Mobile number must be a positive number")
+    .integer("Mobile number must be an integer")
+    .required("Mobile Number is required")
+    .test(
+      "len",
+      "Mobile number must be exactly 10 digits",
+      (val) => val.toString().length === 10
+    ),
 });
 
-
 const otpSchema = yup.object().shape({
-  otp1: yup.string().required("otp is required").matches(/^\d{1}$/),
-  otp2: yup.string().required("otp is required").matches(/^\d{1}$/),
-  otp3: yup.string().required("otp is required").matches(/^\d{1}$/),
-  otp4: yup.string().required("otp is required").matches(/^\d{1}$/),
-
-})
-
-export { loginSchema,otpSchema
-     };
+      otp:  yup.string()
+      .typeError("Otp must be a number")
+      .required("Otp is required")
+      .test(
+        "len",
+        "otp must be 4 digit",
+        (val) => val.toString().length === 4
+      ),
+    })
+ 
+export { loginSchema, otpSchema };
