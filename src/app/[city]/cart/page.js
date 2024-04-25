1f6a104712fae0ada9a9409ecc39084a76dcd9bd
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-25 14:00:25 +0530

    removecart added

--- src/app/[city]/cart/page.js
+++ src/app/[city]/cart/page.js
@@ -183,7 +183,7 @@ const page = ({ params }) => {
                                       )}
                                     </h4>
                                     <div className="trash-icon-div">
-                                      <img className="" src="https://fama.b-cdn.net/RnB/gold-trash%20(2).png" onClick={removeFromCart}></img>
+                                      <img className="" src="https://fama.b-cdn.net/RnB/gold-trash%20(2).png" onClick={()=>removeFromCart(item.cp_id, item.cost)}></img>
                                     </div>
                                   </div>
                                 </div>
