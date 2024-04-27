49ba45acbadbbf7bfa024674829bce1b4a5465a1
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-27 12:38:20 +0530

    new changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,9 @@
-import { redirect } from 'next/navigation'
- 
-async function fetchTeam(city) {
-     console.log("city",city)
-     if(city !==""){
-      return city
-     }
-}
- 
-export default async function Profile({ params }) {
-  const city = await fetchTeam(params.city)
-  if (!city) {
-    redirect('/mumbai')
-  }else{
-    redirect('/'+city)
+"use client"
+import { useRouter } from 'next/navigation';
 
-  }
-
+export default function Profile() {
+    const router = useRouter();
+    
+    router.push("/mumbai");
+        return null;
 }
