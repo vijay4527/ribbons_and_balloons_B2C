6082b5e3bda28934c877504a397b3880389c2467
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-29 19:02:27 +0530

    new changes

--- src/app/[city]/p/[productbyname]/page.js
+++ src/app/[city]/p/[productbyname]/page.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { axiosGet, axiosPost, axiosGetAll } from "@/api";
 import Product from "@/components/product";
-import AppConfig from "@/AppConfig";
 import styles from "@/app/[city]/p/[productbyname]/page.module.css";
 import homeStyles from "@/app/home.module.css";
 import Link from "next/link";
+import AppConfig from "@/AppConfig";
 import ProductImages from "@/components/productImages";
-import AddToCart from "@/components/addToCartButton"
-import ProducDetails from "@/components/productDetails";
-
+import AddToCart from "@/components/addToCartButton";
+import ProductDetails from "@/components/productDetails";
+import AddToFavoritesButton from "@/components/AddToFavoritesButton";
+import CategoryStyles from "@/app/[city]/l/[category]/page.module.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 export async function generateMetadata({ params }) {
   const data = await GetProductData(params.productbyname, params.city);
   if (data) {
-
     return {
       title: data.product_name,
       description:
@@ -34,6 +37,33 @@ export async function generateMetadata({ params }) {
     };
   }
 }
+
+async function getCategoryData(category, city) {
+  try {
+    if (category) {
+      const obj = {
+        category_name: category || "",
+        sub_category_name: "",
+        city_name: city,
+      };
+      const categorydata = await axiosPost(
+        "/ProductMaster/GetB2CProducts",
+        obj
+      );
+      if (categorydata) {
+        return categorydata;
+      }
+    }
+
+    return {
+      data: response,
+      category: category,
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return "";
+  }
+}
 async function GetProductData(productname, city) {
   const Name = productname.split("-").join(" ");
   try {
@@ -41,6 +71,10 @@ async function GetProductData(productname, city) {
       `/productMaster/GetProductByName/${city}/${Name}`
     );
     if (response) {
+      // const categoryProduct = await getCategoryData(response, city);
+      // if (categoryProduct) {
+      //   console.log("categoryProduct", categoryProduct);
+      // }
       return response;
     }
   } catch (error) {
@@ -48,121 +82,137 @@ async function GetProductData(productname, city) {
   }
 }
 const productbyname = async ({ params }) => {
+  var respObject = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
   const city = params.city;
   const productname = params.productbyname;
   const data = await GetProductData(productname, city);
-  let image = data.product_image.split(","); 
-  return (
-    <>
-      {data ? (
-        <>
-          <div className={styles.common_header}>
-            <div className={homeStyles["container_fluid"]}>
-              <div className={styles.content_heading}>
-                <div className={styles.content_title_heading}>
-                  <span className={styles.back_to_shop}>READY REGULARS</span>
-                  <h1 className={styles.text_title_heading}>
-                    <Link
-                      href={`/${city}/l/${
-                        data ? data?.category_name.split(" ").join("-") : ""
-                      }`}
-                      style={{ textDecoration: "none", color: "white" }}
-                    >
-                      {data ? data.category_name : ""}
-                    </Link>
-                  </h1>
-                </div>
-                <div className={styles.breadcrumb}>
+  if (data) {
+    let image = data.product_image.split(",");
+    const categoryProduct = (productname.replaceAll("-", " "), city);
+    if (categoryProduct) {
+      console.logo("categoryProduct", categoryProduct);
+    }
+    return (
+      <>
+        {data && (
+          <div className={styles.pdp_WrapContent}>
+            <div className={styles.common_header}>
+              <div className={homeStyles["container_fluid"]}>
+                <div className={styles.content_heading}>
+                  <div className={styles.content_title_heading}>
+                    <span className={styles.back_to_shop}>READY REGULARS</span>
+                    <h1 className={styles.text_title_heading}>
+                      <a
+                        href={`/${city}/l/${
+                          data ? data?.category_name.split(" ").join("-") : ""
+                        }`}
+                        style={{ textDecoration: "none", color: "white" }}
+                      >
+                        {data ? data.category_name : ""}
+                      </a>
+                    </h1>
+                  </div>
                   <div className={styles.breadcrumb}>
-                    <Link
-                      href={`/${city}/l/${data.category_name
-                        .split(" ")
-                        .join("-")}/${data.sub_category_name
-                        .split(" ")
-                        .join("-")}`}
-                      style={{ textDecoration: "none", color: "white" }}
-                    >
-                      {data ? data.category_name : ""}{" "}
-                      <span className={styles.delimiter}>
-                        {data ? data.sub_category_name : ""}
-                      </span>
-                    </Link>
+                    <div className={styles.breadcrumb}>
+                      <Link
+                        href={`/${city}/l/${data.category_name
+                          .split(" ")
+                          .join("-")}/${data.sub_category_name
+                          .split(" ")
+                          .join("-")}`}
+                        style={{ textDecoration: "none", color: "white" }}
+                      >
+                        {data ? data.category_name : ""}{" "}
+                        <span className={styles.delimiter}>
+                          {data ? data.sub_category_name : ""}
+                        </span>
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-              {data ? (
-        <div className={styles.pdp_Wrap}>
-          <div className={homeStyles["container_fluid"]}>
-            <div className={styles.pdp_DetailBody}>
-              <div className={styles.pdp_detailImgs}>
-                <div className={styles.pdp_DetailImg}>
-                  {image ? (
-                    <ProductImages data={data} city={city}/>
-                  ) : (
-                    <p>No images available</p>
-                  )}
+            {data ? (
+              <div className={styles.pdp_Wrap}>
+                <div className={homeStyles["container_fluid"]}>
+                  <div className={styles.pdp_DetailBody}>
+                    <div className={styles.pdp_detailImgs}>
+                      <ProductImages data={data} images={image} city={city} />
+                    </div>
+                    <div className={styles.pdp_detailDesc}>
+                      <div className={styles.pdp_DetailDesc}>
+                        {data ? (
+                          <>
+                            <h2 className={styles.pdp_ProductName}>
+                              {data.product_name}
+                            </h2>
+                            <div className={styles.underLineSeperator}></div>
+                            <AddToCart data={data} city={city}></AddToCart>
+                          </>
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                      {data && <ProductDetails data={data}></ProductDetails>}
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.pdp_otherContent}>
+                  <div className={homeStyles["container_fluid"]}>
+                    <div className={styles.reviewSection}>
+                      {data ? (
+                        <div className="ShowCaseSliderWrap">
+                          <div className="ShowCaseSliderTitle">
+                            You may also like
+                          </div>
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className={styles.pdp_detailDesc}>
-                
-                <div className={styles.pdp_DetailDesc}>
-                  {data ? (
-                    <>
-                      <h2 className={styles.pdp_ProductName}>
-                        {data.product_name}
-                      </h2>
-                      <div className={styles.underLineSeperator}></div>
-                      <AddToCart data={data} city={city}></AddToCart>
-                    </>
-                  ) : (
-                    ""
-                  )}
-                 
-                </div>
-                {
-                  data && (
-                    <ProducDetails data={data}></ProducDetails>
-                  )
-                }
-              </div>
-            </div>
+            ) : (
+              <>
+                <img
+                  src="https://static.vecteezy.com/system/resources/thumbnails/006/549/647/small/404-landing-page-free-vector.jpg"
+                  alt="No image found"
+                />
+                <span>Product not found</span>
+              </>
+            )}
           </div>
-          {/* <div className={styles.pdp_otherContent}>
-            <div className={homeStyles["container_fluid"]}>
-              <div className={styles.reviewSection}>
-                {
-                  data ? (
-                    <ShowCaseSlider
-                    sliderName="You may also like"
-                    sliderData={data}
-                    city={city}
-                  />
-                  ) : (
-                    ""
-                  )
-                }
-              </div>
-            </div>
-          </div> */}
-        </div>
-      ) : (
-        <>
-          <img
-            src="https://static.vecteezy.com/system/resources/thumbnails/006/549/647/small/404-landing-page-free-vector.jpg"
-            alt="No image found"
-          />
-          <span>Product not found</span>
-        </>
-      )}
-          {/* <Product data={data} city={city} /> */}
-        </>
-      ) : (
-        ""
-      )}
-    </>
-  );
+        )}
+      </>
+    );
+  }
 };
 
 export default productbyname;
--- src/components/ShowCaseSlider.js
+++ src/components/ShowCaseSlider.js
@@ -1,108 +1,116 @@
-import React from "react";
-import Head from "next/head";
-import styles from "@/app/[city]/l/[category]/page.module.css";
-import Link from "next/link";
-import AppConfig from "@/AppConfig";
-import { axiosPost } from "@/api";
-import AddToFavoritesButton from "./AddToFavoritesButton";
-import "react-multi-carousel/lib/styles.css"; 
-import Carousel from "react-multi-carousel";
+// import React from "react";
+// import Head from "next/head";
+// import styles from "@/app/[city]/l/[category]/page.module.css";
+// import Link from "next/link";
+// import AppConfig from "@/AppConfig";
+// import { axiosPost } from "@/api";
+// import AddToFavoritesButton from "./AddToFavoritesButton";
+// import "react-multi-carousel/lib/styles.css"; 
+// import Carousel from "react-multi-carousel";
 
-async function getCategoryData(sliderData,city) {
-    try {
-      if (sliderData.category_name) {
-        const obj = {
-          category_name: sliderData.category_name || "",
-          sub_category_name: "",
-          city_name: city,
-        };
-        const getData = await axiosPost("/ProductMaster/GetB2CProducts", obj);
-        if (getData) {
-          console.log("category",getData)
-         return  getData;
-        }
-      } 
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        return {
-            data: null,
-            additionalData: null,
-        };
-      }
-  }
+// async function getCategoryData(sliderData,city) {
+//     try {
+//       if (sliderData.category_name) {
+//         const obj = {
+//           category_name: sliderData.category_name || "",
+//           sub_category_name: "",
+//           city_name: city,
+//         };
+//         const getData = await axiosPost("/ProductMaster/GetB2CProducts", obj);
+//         if (getData) {
+//          return getData;
+//         }
+//       }
+       
+//           return {
+//             data: response,
+//             category: category,
+//           };
+//       } catch (error) {
+//         console.error("Error fetching data:", error);
+//         return {
+//             data: null,
+//             additionalData: null,
+//         };
+//       }
+//   }
 
-const ShowCaseSlider = async({ sliderName, sliderData, city }) => {
-  const categoryProduct =await getCategoryData(sliderData,city)
-  return (
-    <>
-      <div className="ShowCaseSliderWrap">
-        <div className="ShowCaseSliderTitle">{sliderName}</div>
-        <div className="ShowCaseSliderBody">
-          {category && categoryProduct.length > 0 ? (
-            <Carousel
-              responsive={{
-                superLargeDesktop: {
-                  breakpoint: { max: 4000, min: 3000 },
-                  items: 5,
-                },
-                desktop: {
-                  breakpoint: { max: 3000, min: 1024 },
-                  items: 4,
-                },
-                tablet: {
-                  breakpoint: { max: 1024, min: 464 },
-                  items: 2,
-                },
-                mobile: {
-                  breakpoint: { max: 464, min: 0 },
-                  items: 1,
-                },
-              }}
-              ssr={true} 
-              autoPlay={true}
-              autoPlaySpeed={2000}
-              containerClass="carousel-container" 
-              dotListClass="custom-dot-list"
-              itemClass="Custom-class" 
-            >
-              {categoryProduct.map((item, index) => {
-                const productName = item.product_name.split(" ").join("-");
-                var image = item.product_image.split(",");
-                return (
-                  <Link
-                    key={item.product_id}
-                    href={`/mumbai/p/${productName}`}
-                    className={styles.itemCard}
-                    prefetch={true}
-                  >
-                    <div className={styles.item}>
-                      <div className={styles.itemInfo}>
-                        <AddToFavoritesButton productData={item} />
-                        <div className={styles.imgHvr}>
-                          <img
-                            className={styles.plpProdctImg}
-                            src={`${AppConfig.cdn}products/${image[0]}`}
-                            alt="No image found dhddsjdks"
-                          />
-                        </div>
-                        <div className={styles.itemDesc}>
-                          <h1>{item.product_name}</h1>
-                          <h4>Sinful Collections</h4>
-                          <p>₹ {item.cost}</p>
-                        </div>
-                      </div>
-                    </div>
-                  </Link>
-                );
-              })}
-            </Carousel>
-          ) : (
-            <p>No data available</p>
-          )}
-        </div>
-      </div>
-    </>
-  );
-};
+// const ShowCaseSlider = async({ sliderName, sliderData, city }) => {
+  
+//  var respObject = {
+//     superLargeDesktop: {
+//       breakpoint: { max: 4000, min: 3000 },
+//       items: 5,
+//     },
+//     desktop: {
+//       breakpoint: { max: 3000, min: 1024 },
+//       items: 4,
+//     },
+//     tablet: {
+//       breakpoint: { max: 1024, min: 464 },
+//       items: 2,
+//     },
+//     mobile: {
+//       breakpoint: { max: 464, min: 0 },
+//       items: 1,
+//     },
+//  }
 
-export default ShowCaseSlider;
+//   const categoryProduct =await getCategoryData(sliderData,city)
+
+//   return (
+//     <>
+//       <div className="ShowCaseSliderWrap">
+//         <div className="ShowCaseSliderTitle">{sliderName}</div>
+//         <div className="ShowCaseSliderBody">
+//           {categoryProduct.length > 0 ? (
+//             <Carousel
+//               responsive={respObject}
+//               ssr={true} 
+//               autoPlay={true}
+//               autoPlaySpeed={2000}
+//               containerClass="carousel-container" 
+//               dotListClass="custom-dot-list"
+//               itemClass="Custom-class" 
+//             >
+//               {categoryProduct.map((item, index) => {
+//                 const productName = item.product_name.split(" ").join("-");
+//                 var image = item.product_image.split(",");
+//                 return (
+//                   <Link
+//                     key={item.product_id}
+//                     href={`/mumbai/p/${productName}`}
+//                     className={styles.itemCard}
+//                     prefetch={true}
+//                   >
+//                     <div className={styles.item}>
+//                       <div className={styles.itemInfo}>
+//                         <AddToFavoritesButton productData={item} />
+//                         <div className={styles.imgHvr}>
+//                           <img
+//                             className={styles.plpProdctImg}
+//                             src={`${AppConfig.cdn}products/${image[0]}`}
+//                             alt="No image found dhddsjdks"
+//                           />
+//                         </div>
+//                         <div className={styles.itemDesc}>
+//                           <h1>{item.product_name}</h1>
+//                           <h4>Sinful Collections</h4>
+//                           <p>₹ {item.cost}</p>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </Link>
+//                 );
+//               })}
+//             </Carousel>
+//           ) : (
+//             <p>No data available</p>
+//           )}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ShowCaseSlider;
