01f5adc06a545d8ab52d6388155f3ac802d6de03
vijay4527 <vijaysaroj4527@gmail.com>
2024-06-05 19:02:50 +0530

    new changes

--- src/utils/commoncity.js
+++ src/utils/commoncity.js
@@ -3,9 +3,9 @@ import { axiosGet, axiosPost } from "@/api";
   export async function getCities() {
     try {
       const response = await axiosGet('/RNBCity/GetAllRNBCity');
-      return response; // Assuming the cities data is in the response.data
+      return response; 
     } catch (error) {
       console.error('Error fetching data:', error);
-      return null; // Indicate error or handle differently as needed
+      return null; 
     }
   }
