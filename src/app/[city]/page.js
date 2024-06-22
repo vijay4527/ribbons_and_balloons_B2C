801f33d8442228b41d931e59ea5c6017d8a0e72f
vijay4527 <vijaysaroj4527@gmail.com>
2024-06-22 13:46:22 +0530

    new changes

--- src/app/[city]/page.js
+++ src/app/[city]/page.js
@@ -101,6 +101,40 @@ const page = async ({ params }) => {
       <Testimonials />
       <InstaPosts city={city} data={media} />
       <NewLaunches city={city} data={media?.New_Launches} />
+      {/* <div className="cakeOfMonthWrap">
+        <div className="headerTitle">
+          <h2>Cake of the month</h2>
+          <div className="testimonialUnderLine">
+            <div className="testimonialUnder">
+              <div className="ux`nderLine"></div>
+              <div className="shapLine"></div>
+            </div>
+          </div>
+        </div>
+        <div className="cakeOfMonthBody">
+          <div className="wrapper">
+            <video
+              muted
+              autoPlay
+              loop
+              className="backdrop"
+              style={{ width: "100%" }}
+            >
+              <source
+                src="https://fama.b-cdn.net/PalmExpo/palmExpo.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div className="backdrop"></div>
+            <div className="stage_floor"></div>
+            <div className="stage_highlight"></div> 
+             <div className="spotlight_swivel">
+              <div className="lamp"></div>
+              <div className="spotlight"></div>
+            </div>
+          </div>
+        </div>
+      </div> */}  
       <CakeOfTheMonth city={city} data={media?.Cake_Of_The_Month} />
       <MediaCollaborators city={city} data={media?.Media_Collaborator} />
       <div className="enquiryWrapper">
