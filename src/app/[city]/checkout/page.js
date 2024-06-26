e3818709fcc6225a907f23b421976782ddad4a7e
Arzan Khan <arzankhan889@gmail.com>
2024-06-26 18:39:29 +0530

    changes

--- src/app/[city]/checkout/page.js
+++ src/app/[city]/checkout/page.js
@@ -923,9 +923,9 @@ const page = ({ params }) => {
                 <div className="" style={{ width: "100%", marginTop: "10px" }}>
                   <div className={styles.cartPriceBox}>
                     <div
-                      className={`${styles.cartOrderSummary} d-flex justify-content-center`}
+                      className={`${styles.cartOrderSummary} m-0`}
                     >
-                      <h4
+                      <h4 className={styles.addCouponItem}
                         onClick={() => {
                           setModalIsOpen(true);
                         }}
@@ -945,10 +945,10 @@ const page = ({ params }) => {
         </div>
       </section>
       <Modal
-        id="login-modal-dialog"
+        id="coupons-modal-dialog"
         show={modalIsOpen}
         onHide={closeModal}
-        className={homeStyles["loginModal"]}
+        className={homeStyles["couponsModal"]}
         centered
       >
         <div className={styles.modalBox}>
@@ -964,73 +964,78 @@ const page = ({ params }) => {
                 onChange={(e) => handleCouponChange(e.target.value)}
                 placeholder="Search Coupon if any"
               />
+              <div className={styles.couponBody}>
               {filteredCoupon.length > 0 &&
                 filteredCoupon.map((res) => (
                   <>
-                    <label
-                      htmlFor={`Franchise${res.coupon_id}`}
-                      className={`${styles.pickUpSearchResultItem} ${
-                        selectedCoupon === res.coupon_id
-                          ? `${styles.active}`
-                          : ""
-                      }`}
-                      key={res.coupon_id}
-                    >
-                      <div className={styles.pickUpFranchiseInput}>
-                        <input
-                          id={`Franchise${res.coupon_id}`}
-                          className="form-check-input"
-                          type="radio"
-                          value="pickup"
-                          checked={selectedCoupon === res.coupon_id}
-                          onChange={() => {
-                            hanldeCoupon(res);
-                          }}
-                        />
-                        <div className={styles.pickUpFranchiseInputIcon}>
-                          <svg
-                            className={styles.roundedIcon}
-                            focusable="false"
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                          >
-                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
-                          </svg>
-                          <svg
-                            className={styles.solidIcon}
-                            focusable="false"
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                          >
-                            <path d="M8.465 8.465C9.37 7.56 10.62 7 12 7C14.76 7 17 9.24 17 12C17 13.38 16.44 14.63 15.535 15.535C14.63 16.44 13.38 17 12 17C9.24 17 7 14.76 7 12C7 10.62 7.56 9.37 8.465 8.465Z"></path>
-                          </svg>
-                        </div>
-                      </div>
-                      <div
-                        className={`${styles.pickUpFranchiseDetails} ${styles.couponDetails}`}
+                    <div className={styles.couponItem}>
+                      <label
+                        htmlFor={`Franchise${res.coupon_id}`}
+                        className={`${styles.pickUpSearchResultItem} ${
+                          selectedCoupon === res.coupon_id
+                            ? `${styles.active}`
+                            : ""
+                        }`}
+                        key={res.coupon_id}
                       >
-                        <div className={styles.modalCouponName}>
-                          <h4>{res.coupon_name}</h4>
+                        <div className={styles.pickUpFranchiseInput}>
+                          <input
+                            id={`Franchise${res.coupon_id}`}
+                            className="form-check-input"
+                            type="radio"
+                            value="pickup"
+                            checked={selectedCoupon === res.coupon_id}
+                            onChange={() => {
+                              hanldeCoupon(res);
+                            }}
+                          />
+                          <div className={styles.pickUpFranchiseInputIcon}>
+                            <svg
+                              className={styles.roundedIcon}
+                              focusable="false"
+                              viewBox="0 0 24 24"
+                              aria-hidden="true"
+                            >
+                              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
+                            </svg>
+                            <svg
+                              className={styles.solidIcon}
+                              focusable="false"
+                              viewBox="0 0 24 24"
+                              aria-hidden="true"
+                            >
+                              <path d="M8.465 8.465C9.37 7.56 10.62 7 12 7C14.76 7 17 9.24 17 12C17 13.38 16.44 14.63 15.535 15.535C14.63 16.44 13.38 17 12 17C9.24 17 7 14.76 7 12C7 10.62 7.56 9.37 8.465 8.465Z"></path>
+                            </svg>
+                          </div>
                         </div>
-                        {selectedCoupon === res.coupon_id &&
-                          displayCancelButton && (
-                            <div className={`${styles.pickUpFranchiseInfo}`}>
-                              <h6>{couponMessage}</h6>
-                            </div>
-                          )}
-                      </div>
-                    </label>
-                    {selectedCoupon === res.coupon_id &&
-                      displayCancelButton && (
-                        <button
-                          className="btn btn-primary"
-                          onClick={removeCoupon}
+                        <div
+                          className={`${styles.pickUpFranchiseDetails} ${styles.couponDetails}`}
                         >
-                          Remove
-                        </button>
-                      )}
+                          <div className={styles.modalCouponName}>
+                            <h4>{res.coupon_name}</h4>
+                          </div>
+                          {selectedCoupon === res.coupon_id &&
+                            displayCancelButton && (
+                              <div className={`${styles.pickUpFranchiseInfo}`}>
+                                <h6>{couponMessage}</h6>
+                              </div>
+                            )}
+                        </div>
+                      </label>
+                      
+                      {selectedCoupon === res.coupon_id &&
+                        displayCancelButton && (
+                          <button
+                              className="btn btn-primary"
+                            onClick={removeCoupon}
+                          >
+                            Remove
+                          </button>
+                        )}
+                    </div>
                   </>
                 ))}
+              </div>
             </div>
             {isBtnVisible && (
               <div className="btnCoupon">
